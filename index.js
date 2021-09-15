bad3a924dd1d3cf91e321999afcadcc7a0010539
Mike Herrera <michael.herrera@gmail.com>
2021-09-15 14:39:23 -0700

    Start the process of segmenting teams by availability

--- index.js
+++ index.js
@@ -4,5 +4,18 @@ import { getNflGames, getNflStandings } from "./data-fivethirtyeight.js";
 const games = await getNflGames();
 const standings = await getNflStandings();
 
-// deepLog(games);
-deepLog(standings);
+const teams = Object.values(standings);
+const teamNames = teams.map(t => t.team);
+
+/**
+ * teamsSelected represents any teams that have been selected in earlier rounds
+ * of the competition. When we search for our optimal solution we will ignore
+ * these teams.
+ * 
+ * This array should be empty if we were pre-competition.
+ */
+const teamsSelected = [
+    'Panthers',
+    'Packers'
+];
+const teamsAvailable = new Set(teamNames.filter(team => !teamsSelected.includes(team)));
