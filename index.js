ba98a1475aa0239bb1be2472f1269e31c79ace18
Mike Herrera <michael.herrera@gmail.com>
2021-09-15 23:08:36 -0700

    WIP Build interesting game methods

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import { deepLog } from "./data-utils.js";
 import { getNflGames, getNflStandings } from "./data-fivethirtyeight.js";
 
-const games = await getNflGames();
-const standings = await getNflStandings();
+let seasonGames = await getNflGames();
 
+const getGamesForWeek = (weekNumber) => seasonGames[weekNumber].games;
+const getGameSpreadFavoredTeam = (game) => {
+    let favoredTeam = game.teams.find( (t) => Number(t.spread) < 0);
+    return favoredTeam;
+};
+
+
+const getTeamNamesOfSpreadFavoredGames = (games) => {
+    games.map((game) => {
+        const favoredTeam = getGameSpreadFavoredTeam(game);
+        favoredTeam.team;
+    });
+};
+
+
+const standings = await getNflStandings();
 const teams = Object.values(standings);
 const teamNames = teams.map(t => t.team);
 
@@ -14,8 +29,16 @@ const teamNames = teams.map(t => t.team);
  * 
  * This array should be empty if we were pre-competition.
  */
-const teamsSelected = [
+const teamNamesOfSelected = [
     'Panthers',
     'Packers'
 ];
-const teamsAvailable = new Set(teamNames.filter(team => !teamsSelected.includes(team)));
+const teamNamesOfAvailable = new Set(teamNames.filter(team => !teamNamesOfSelected.includes(team)));
+
+
+let week2Games = getGamesForWeek(2);
+let game = week2Games[0];
+
+let winners = getTeamNamesOfSpreadFavoredGames(week2Games);
+
+console.log('done')
